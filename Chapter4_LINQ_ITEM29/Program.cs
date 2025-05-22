23651becf0bc71afd5d401a56b69335cfdfe2da5
junPark <iii211sdf@naver.com>
2025-05-22 20:16:22 +0900

    최신화

--- Chapter4_LINQ_ITEM29/Program.cs
+++ Chapter4_LINQ_ITEM29/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Chapter4_LINQ_ITEM29
+﻿using System.Net.Sockets;
+
+namespace Chapter4_LINQ_ITEM29
 {
     internal class Program
     {
@@ -59,7 +61,7 @@
             var count = 0;
             foreach (T item in Source)
             {
-                if(++count % period == 0)
+                if (++count % period == 0)
                 {
                     yield return item;
                 }
@@ -67,13 +69,57 @@
         }
 
         //아래 메서드는 새로운 시퀀스를 생성하는 변환 메서드 예시
-        public static IEnumerable<T> Select<T>(IEnumerable<T> Source,Func<T,T> methoc)
+        public static IEnumerable<T> Select<T>(IEnumerable<T> Source, Func<T, T> methoc)
         {
-            foreach(T t in Source)
+            foreach (T t in Source)
             {
                 yield return methoc(t);
             }
         }
+
+        //아래 이터레이터 메서드는 새로운 시퀀스를 생성하는 메서드이다. 매개변수가 시퀀스가 아닌 형태임
+        //또한 다른 메서드와 조합하여 순회를 중단할 수 있음
+        public static IEnumerable<int> CreateSequnece(int numberOfElements, int startAt, int stepBy)
+        {
+            for (var i = 0; i < numberOfElements; i++)
+            {
+                yield return startAt + i * stepBy;
+            }
+
+
+#if DEBUG
+            //호출자 예시
+            var listStorage = new List<int>(CreateSequnece(100, 0, 5));
+            var sequence = CreateSequnece(10000, 0, 7).TakeWhile(delegate (int num) { return num < 1000; });
+            var sequence2 = CreateSequnece(10000, 0, 7).TakeWhile(num => num < 1000);
+#endif
+        }
+
+        //아래 메서드는 CreateSquence에 매개변수로 출력 시퀀스를 만들기 위한 함수로 받는 버전이다
+        public static IEnumerable<T> CreateSequnece<T>(int numberOfElements, Func<T> generator)
+        {
+            for (var i = 0; i < numberOfElements; i++)
+            {
+                yield return generator();
+            }
+
+#if DEBUG
+            int startAt = 0;
+            //호출자 예시
+            var listStorage = new List<int>(CreateSequnece(100, () => startAt += 5));
+#endif
+        }
+
+        public static TResult Fold<T, TResult>(IEnumerable<T> sequence, TResult total, Func<T, TResult, TResult> ACCUMULATOR)
+        {
+            foreach (T item in sequence)
+            {
+                total = ACCUMULATOR(item, total);
+            }
+            return total;
+
+        }
+
     }
     public static class Extensions
     {
